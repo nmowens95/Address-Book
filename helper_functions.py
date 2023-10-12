6e165443bf16ccf07db76432e10e05483c90b7b4
Nathan Owens <nmowens95@gmail.com>
2023-10-12 16:36:02 -0500

    Made changes for aestethic on main, back and forth changes for helper_functions, can add in names

--- helper_functions.py
+++ helper_functions.py
@@ -1,26 +1,38 @@
 import json
 
-def create_file():
-    pass
 
 def add_contact():
     name = input("Name: ")
     number = input("Number: ")
     email = input("Email: ")
-    sub_direct = [name, number, email]
-    new_contact = {name: sub_direct}
+    sub_contact = [name, number, email]
+    full_contact = {name: sub_contact}
     file = open("contacts.txt", "a+")
-    file.write(json.dumps(new_contact))
+    file.write(json.dumps(full_contact))
     file.write("\n")
     file.close()
 
 
-def delete_contact():
-    pass
+def delete_contact(current_contacts):
+    with open("contacts.txt", "r") as r:
+        lines = r.readlines()
+    with open("contacts.txt", "w") as w:
+        for line in lines:
+            if line.strip("\n") != current_contacts:
+                w.write(line)
 
-def search_contact():
-    pass
 
+def search_contact(current_contacts):
+    with open("contacts.txt", "r"):
+        for current in current_contacts:
+            try:
+                if current == current_contacts:
+                    return current
+            except Exception as err:
+                print(err)
+                break
+                
+                    
 def exit_program():
     print("\n--Exiting contacts--\n")
     return exit()
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from helper_functions import *
 
 
-def prompt():
+def command_prompt():
     command = input("Pick: a/d/s/q: ")
+    print()
     if command == "q":
         return exit_program()
     elif command == "a":
         return add_contact()
     elif command == "d":
-        #return delete_contact()
-        print("\ndelete\n")
+        return delete_contact()
     elif command == "s":
         #return search_contact()
         print("\nsearch\n")
@@ -23,8 +23,9 @@ def main():
         print("\n'a' - to add contact")
         print("'d' - to delete contact")
         print("'s' - to search contact")
-        print("'q' - to exit contact book")
+        print("'q' - to exit contact book\n")
         print()
-        prompt()
+
+        command_prompt()
         
 main()
