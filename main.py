dcd3bd4d0391b2792e9eba4b5c8fa959b852a31c
Nathan Owens <nmowens95@gmail.com>
2023-10-11 19:50:53 -0500

    Started to add functions and refactor code

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
 from helper_functions import *
 
+
 def prompt():
     command = input("Pick: a/d/s/q: ")
-    if command == "a":
-        #return add_contact()
-        print("add")
+    if command == "q":
+        return exit_program
+    elif command == "a":
+        return add_contact()
     elif command == "d":
         #return delete_contact()
-        print("delete")
+        print("\ndelete\n")
     elif command == "s":
         #return search_contact()
-        print("search")
-    if command == "q":
-        #return exit_program()
-        quit
-
-
+        print("\nsearch\n")
+    else:
+        print("Try a prompted command \n")
+    
 
 def main():
     while True:
-        print("'a' to add contact")
-        print("'d' to delete contact")
-        print("'s' to search contact")
-        print("'q' to exit contact book")
-        
-        try:
-            prompt()
-        except:
-            print("Invalid command")
-            continue
+        print("'a' - to add contact")
+        print("'d' - to delete contact")
+        print("'s' - to search contact")
+        print("'q' - to exit contact book")
+        print()
+        prompt()
         
 main()
