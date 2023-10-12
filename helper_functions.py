6693158c255b26c8bd46c8c9fd9e242d044bbd8d
Nathan Owens <nmowens95@gmail.com>
2023-10-11 19:49:46 -0500

    Started input to add_contacts and opening into new document

--- helper_functions.py
+++ helper_functions.py
@@ -2,7 +2,16 @@ def create_file():
     pass
 
 def add_contact():
-    pass
+    name = input("Name: ")
+    number = input("Number: ")
+    email = input("Email: ")
+    sub_direct: {}
+    new_contact: {name: sub_direct}
+    with open('contacts.txt') as reader:
+        pass # append to list and add to document
+
+
+
 
 def delete_contact():
     pass
@@ -11,4 +20,5 @@ def search_contact():
     pass
 
 def exit_program():
-    pass
+    print("\n--Exiting contacts--\n")
+    return exit()
